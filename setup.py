e4f27b85fc06cb2d0717fad0905c43baa02c6c38
Marios S. Kyriakou <mariosmsk@gmail.com>
2023-05-17 15:21:07 +0300

    update requirements

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['Click>=7.0', 'openai', 'pandas', 'pypdf']
+requirements = ['Click>=7.0', 'openai', 'pandas', 'pypdf', 'matplotlib', 'plotly', 'scipy', 'scikit-learn']
 
 test_requirements = [ ]
 
